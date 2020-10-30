b09766125b46e0c12684adf076c7c02a1c24c3d0
Nathan Stratford <nstratford76@gmail.com>
2020-10-29 18:35:34 -0600

    Add project files.

---
+++ Models/Scripture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ScriptureID { get; set; }
+
+        public string Canon { get; set; }
+
+        public string Book { get; set; }
+
+        public string Chapter { get; set; }
+
+        public string Verses { get; set; }
+
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
